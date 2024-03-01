bfd75b4e17cf64272103b701d5543c9e4ffb4e6d
Jesper Højlund <github@hillgrove.dk>
2024-03-01 22:36:17 +0100

    Solved N120

---
+++ Niveau 1 - Grundlæggende C#/N120 Brug af strenge/Program.cs
@@ -0,0 +1,20 @@
+﻿
+// To make console output danish letters
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+string fornavn = "Jesper";
+string efternavn = "Højlund";
+string samletNavn = fornavn + " " + efternavn;
+Console.WriteLine(samletNavn);
+
+string navnStort = samletNavn.ToUpper();
+Console.WriteLine(navnStort);
+
+string navnLille = samletNavn.ToLower();
+Console.WriteLine(navnLille);
+
+string navnSubstring1 = samletNavn.Substring(efternavn.Length, 3);
+Console.WriteLine(navnSubstring1);
+
+string navnSubstring2 = char.ToUpper(samletNavn[^4]) + samletNavn.Substring(samletNavn.Length - 3);
+Console.WriteLine(navnSubstring2);
