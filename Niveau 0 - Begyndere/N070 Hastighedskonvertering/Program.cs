91072690c6340e99e6a699e78ab2f05b2a81ab7c
Jesper Højlund <github@hillgrove.dk>
2024-03-01 17:34:44 +0100

    Solved Niveau 0 exercises

---
+++ Niveau 0 - Begyndere/N070 Hastighedskonvertering/Program.cs
@@ -0,0 +1,27 @@
+﻿
+Console.Write("Indtast hastighed in MPH: ");
+string? input = Console.ReadLine();
+
+try
+{
+    double mph = double.Parse(input);
+    if (mph < 0 )
+    {
+        Console.WriteLine("Hastigheden kan ikke være negativ");
+        return;
+    }
+
+    double kmt = mphToKmt(mph);
+    Console.WriteLine($"Resultat: {mph} MPH er lig med {kmt:F2} KMT.");
+}
+
+catch (FormatException)
+{
+    Console.WriteLine("Input var ikke et gyldigt tal - programmet afsluttes");
+}
+
+
+double mphToKmt(double mph)
+{
+    return mph * 1.60934;
+}
