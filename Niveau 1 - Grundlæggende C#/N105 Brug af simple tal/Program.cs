055d66a5fa1addc7a343a90accca92f0a7d2ec1c
Jesper Højlund <github@hillgrove.dk>
2024-03-01 21:10:53 +0100

    Solved N105

---
+++ Niveau 1 - Grundlæggende C#/N105 Brug af simple tal/Program.cs
@@ -0,0 +1,12 @@
+﻿
+int heltal = 10;
+heltal++;
+heltal = heltal - 1;
+heltal += 20;
+Console.WriteLine(heltal);
+
+double kommatal = 12.5;
+kommatal++;
+kommatal = kommatal - 1;
+kommatal *= 2;
+Console.WriteLine(kommatal);
