670c4264e4889bdef38bfa1e833c648384335e0d
Jesper Højlund <github@hillgrove.dk>
2024-03-01 22:07:53 +0100

    Solved N115

---
+++ Niveau 1 - Grundlæggende C#/N115 Typekonvertering/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.Write("Indtast tal 1: ");
+string? tal1 = Console.ReadLine();
+Console.Write("Indtast tal 2: ");
+string? tal2 = Console.ReadLine();
+
+double t1 = Convert.ToDouble(tal1);
+double t2 = Convert.ToDouble(tal2);
+double t3 = t1 + t2;
+
+Console.WriteLine($"Resultat: {t3:N2}");
