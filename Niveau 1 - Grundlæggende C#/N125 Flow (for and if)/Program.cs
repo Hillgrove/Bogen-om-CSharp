5b1ae4236d1c73a268faab53d84ffa906d6957f7
Jesper Højlund <github@hillgrove.dk>
2024-03-02 01:09:26 +0100

    Solved N125

---
+++ Niveau 1 - Grundlæggende C#/N125 Flow (for and if)/Program.cs
@@ -0,0 +1,34 @@
+﻿
+for (int i = 0; i < 10; i++)
+{
+    Console.Write($"{i} ");
+}
+
+Console.WriteLine();
+
+for (int i = 1; i <= 20; i += 2)
+{
+    Console.Write($"{i} ");
+}
+
+Console.WriteLine();
+
+for (int i = 0; i < 10; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.Write($"{i} ");
+    }
+
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write($"{i} ");
+    }
+}
+
+Console.ForegroundColor = ConsoleColor.Gray;
+Console.WriteLine();
+
+Console.WriteLine("testing color");
