a66bb9a2ca813055daf5b68f72465309b5b51802
Jesper Højlund <github@hillgrove.dk>
2024-03-02 21:04:48 +0100

    Solved N135

---
+++ Niveau 1 - Grundlæggende C#/N135 Flow (break og continue)/Program.cs
@@ -0,0 +1,14 @@
+﻿
+// Print out 0, 3, 6, 9, 12, 15 with use of continue and break.
+
+
+for (int i = 0; i < 20; i++)
+{
+    if (i % 3 != 0)
+        continue;
+
+    if (i > 15)
+        break;
+
+    Console.Write($"{i} ");
+}
