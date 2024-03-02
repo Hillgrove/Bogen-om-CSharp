0ba197b1feed58b0b9179073e9a96ed9b8b5af94
Jesper Højlund <github@hillgrove.dk>
2024-03-02 21:16:56 +0100

    Solved N140

---
+++ Niveau 1 - Grundlæggende C#/N140 Simple metoder/Program.cs
@@ -0,0 +1,31 @@
+﻿
+int Addition(int a, int b) 
+{ 
+    return a + b; 
+}
+
+double CalcCircleArea(int radius)
+{
+    return (radius * radius * Math.PI);
+}
+
+double CalcMoneyAfterVAT(double money, double vatPercent)
+{
+    return money * (1 - vatPercent / 100);
+}
+
+void Print(string text)
+{ 
+    Console.WriteLine(text); 
+}
+
+int res = Addition(5, 2);
+Console.WriteLine(res); // 7
+
+double res2 = CalcCircleArea(5);
+Console.WriteLine($"{res2:F2}"); // 78.54
+
+double res3 = CalcMoneyAfterVAT(100, 25);
+Console.WriteLine($"{res3:F2}"); // 75
+
+Print("Dette er en test"); // Dette er en test
