0c578143254133d19979df397e265915d7c909fc
Jesper Højlund <github@hillgrove.dk>
2024-03-02 01:32:11 +0100

    Solved N130

---
+++ Niveau 1 - Grundlæggende C#/N130 10-tabel/Program.cs
@@ -0,0 +1,37 @@
+﻿
+/*
+ * Multiplication Table Color Coded
+ * 
+ * This program prints a 10x10 multiplication table to the console.
+ * It changes the color of the output based on the product value:
+ * - Green for products less than 25,
+ * - Yellow for products between 25 and 49, inclusive,
+ * - Red for products 50 and above.
+ * 
+ * Purpose: Demonstrate the use of nested loops, conditional statements,
+ * and console color manipulation in C#.
+ * 
+ * Author: Jesper Højlund
+ * Date: 02/03/2024
+ */
+
+for (int row = 1; row <= 10; row++)
+{
+    for (int col = 1; col <= 10; col++)
+    {
+        int number = row * col;
+
+        Console.ForegroundColor = number switch
+        {
+            < 25 => ConsoleColor.Green,
+            >= 25 and < 50 => ConsoleColor.Yellow,
+            _ => ConsoleColor.Red,
+        };
+        
+        Console.Write($"{number, 5}");
+    }
+
+    Console.WriteLine();
+}
+
+Console.ForegroundColor = ConsoleColor.Gray;
