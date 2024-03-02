4146437b140990c57c13069584001c40295d490f
Jesper Højlund <github@hillgrove.dk>
2024-03-02 22:28:23 +0100

    Solved N157

---
+++ Niveau 1 - Grundlæggende C#/N157 Gæt et tal (procedural version)/Program.cs
@@ -0,0 +1,47 @@
+﻿
+// MVP of a procedural number guessing game.
+
+int randomNumber = GenerateRandomNumber();
+bool gameCompleted = false;
+
+Console.WriteLine("Guess a number between 1 and 100");
+
+while (!gameCompleted)
+{
+    int guess = getNumberFromUser();
+    Feedback(guess);
+}
+
+
+int GenerateRandomNumber()
+{
+    Random rand = new Random();
+    return rand.Next(1, 100);
+}
+
+int getNumberFromUser()
+{
+    Console.Write("Input number: ");
+    string? input = Console.ReadLine();
+    int.TryParse(input, out int number);
+    return number;
+}
+
+void Feedback(int guess)
+{
+    if (guess < randomNumber)
+    {
+        Console.WriteLine("Too small\n");
+    }
+
+    else if (guess > randomNumber)
+    {
+        Console.WriteLine("Too big\n");
+    }
+
+    else 
+    {
+        Console.WriteLine("Congratulations! You guessed it");
+        gameCompleted = true;
+    }
+}
