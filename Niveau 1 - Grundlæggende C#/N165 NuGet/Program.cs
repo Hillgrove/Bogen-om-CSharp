519f60be34a7f523a1d99644d7bed5976a73ffdf
Jesper Højlund <github@hillgrove.dk>
2024-03-03 23:22:09 +0100

    Solved N165

---
+++ Niveau 1 - Grundlæggende C#/N165 NuGet/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using Humanizer;
+
+DateTime nieceBirthday = new DateTime(2000, 7, 9);
+Console.WriteLine($"Niece was born: {nieceBirthday.Humanize()}");
+
+int intNumber = 123;
+Console.WriteLine($"Humanized number ({intNumber}): \"{intNumber.ToWords()}\"");
+
+string testString = "camelCaseString";
+Console.WriteLine(testString.Humanize());
