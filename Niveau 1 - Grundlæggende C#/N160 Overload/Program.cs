99f0311bd8f020adb54e43fc761c601adce1ece5
Jesper Højlund <github@hillgrove.dk>
2024-03-03 22:57:22 +0100

    Solved N160

---
+++ Niveau 1 - Grundlæggende C#/N160 Overload/Program.cs
@@ -0,0 +1,26 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Add(1, 2));
+        Console.WriteLine(Add(1, 2, 3));
+        Console.WriteLine(Add(1, 2, 4, 5));
+    }
+
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static int Add(int a, int b, int c)
+    {
+        return a + b + c;
+    }
+
+    public static int Add(int a, int b, int c, int d)
+    {
+        return a + b + c + d;
+    }
+
+}
