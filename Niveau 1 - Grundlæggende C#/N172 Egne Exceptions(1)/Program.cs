f31aa0075cce5bd2b94eea8e2c10a26ddd2f55e6
Jesper Højlund <github@hillgrove.dk>
2024-04-13 21:41:03 +0200

    Solved N172

---
+++ Niveau 1 - Grundlæggende C#/N172 Egne Exceptions(1)/Program.cs
@@ -0,0 +1,31 @@
+﻿
+static string SmartToUpper(string text)
+{
+    if (text == null)
+    {
+        throw new ArgumentNullException("Tekst må ikke være null");
+    }
+
+    if (text.Length == 0)
+    {
+        throw new ArgumentException("Tekst må ikke være tom");
+    }
+
+    return text.ToUpper();
+}
+
+try
+{
+    Console.WriteLine(SmartToUpper("mikkel"));
+    Console.WriteLine(SmartToUpper("mAtHiAs"));
+
+    // prøv også 
+    Console.WriteLine(SmartToUpper(""));
+    // eller
+    Console.WriteLine(SmartToUpper(null));
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Der er sket en fejl: " + ex.Message);
+}
