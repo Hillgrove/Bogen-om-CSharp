4ff80972039c97382573b860ab24d2de1c68988b
Jesper Højlund <github@hillgrove.dk>
2024-04-14 23:10:00 +0200

    Solved a few small assigments

---
+++ Niveau 1 - Grundlæggende C#/N174 Egne exceptions (2)/Program.cs
@@ -0,0 +1,32 @@
+﻿
+// Setting up variables
+string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+string file = "numbers.txt";
+string filePath = Path.Combine(baseDirectory, file);
+string fileContent;
+
+// Opening file
+if (!File.Exists(filePath))
+{
+    throw new ApplicationException($"The file {filePath} does not exist!");
+}
+
+// Changing content and saving file
+try
+{
+    fileContent = File.ReadAllText(filePath);
+    int number = Convert.ToInt32(fileContent);
+    number ++;
+    File.WriteAllText(filePath, number.ToString());
+    Console.WriteLine($"Number in file is now {number}");
+}
+catch (FormatException)
+{
+    throw new ApplicationException("Content cannot be converted to an integer");
+}
+
+catch (Exception)
+{
+    throw;
+}
+
