a432e4b3e4e3ebe95d4e96ed6954340c33696489
Jesper Højlund <github@hillgrove.dk>
2024-08-18 14:49:43 +0200

    Solved N175

--- Niveau 1 - Grundlæggende C#/N175 Gæt et tal (fejlhåndtering)/Program.cs
+++ Niveau 1 - Grundlæggende C#/N175 Gæt et tal (fejlhåndtering)/Program.cs
@@ -1,1 +1,52 @@
 ﻿
+// MVP of a procedural number guessing game.
+
+int randomNumber = GenerateRandomNumber();
+bool gameCompleted = false;
+
+while (!gameCompleted)
+{
+    int guess = getNumberFromUser();
+    Feedback(guess);
+}
+
+
+int GenerateRandomNumber()
+{
+    Random rand = new Random();
+    return rand.Next(1, 100);
+}
+
+int getNumberFromUser()
+{    
+    try
+    {
+        Console.Write("Guess a number between 1 and 100: ");
+        string input = Console.ReadLine();
+        return Convert.ToInt32(input);
+    }
+    catch (FormatException ex)
+    {
+        Console.WriteLine("Please enter a valid number. " + ex.Message);
+        return getNumberFromUser();
+    }
+}
+
+void Feedback(int guess)
+{
+    if (guess < randomNumber)
+    {
+        Console.WriteLine("Too small\n");
+    }
+
+    else if (guess > randomNumber)
+    {
+        Console.WriteLine("Too big\n");
+    }
+
+    else
+    {
+        Console.WriteLine("Congratulations! You guessed it");
+        gameCompleted = true;
+    }
+}
