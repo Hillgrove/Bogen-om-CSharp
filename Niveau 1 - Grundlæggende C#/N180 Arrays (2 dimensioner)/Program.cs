c0d9a9fdd2e08e1ff0b74872d72e843a40eddb13
Jesper Højlund <github@hillgrove.dk>
2024-08-19 15:17:41 +0200

    Solved N180

---
+++ Niveau 1 - Grundlæggende C#/N180 Arrays (2 dimensioner)/Program.cs
@@ -0,0 +1,55 @@
+﻿
+//Opret en ny tom .NET Core konsol-applikation og definer og opret et array med to dimensioner med følgende indhold:
+
+//id navn
+//“1”	“navn1”
+//“2”	“navn2”
+//“3”	“navn3”
+//“4”	“navn4”
+//Arrayet skal altså være et 4x2 array (4 rækker - 2 kolonner).
+
+using System.Data;
+
+string[,] names = new string[4, 2];
+
+names[0, 0] = "1";
+names[0, 1] = "name1";
+names[1, 0] = "2";
+names[1, 1] = "name2";
+names[2, 0] = "3";
+names[2, 1] = "name3";
+names[3, 0] = "4";
+names[3, 1] = "name4";
+
+static string FindName(string[,] nameArray, string id)
+{
+    string name = string.Empty;
+
+    for (int row = 0; row < nameArray.GetLength(0); row++)
+    {
+        if (nameArray[row, 0] == id)
+        {
+            name = nameArray[row, 1];
+        }
+    }
+
+    return name;
+}
+
+static void PrintArray(string[,] array)
+{
+    Console.WriteLine("The array: ");
+    for (int row = 0; row < array.GetLength(0) ; row++)
+    {
+        for (int col = 0; col < array.GetLength(1); col++)
+        {
+            Console.Write(array[row, col] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+PrintArray(names);
+string name = FindName(names, "2");
+Console.WriteLine($"The name at index 2: {name}");
