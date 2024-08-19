6fb0a6e93c7ae0c1c544821f4565e41618a04711
Jesper Højlund <github@hillgrove.dk>
2024-08-19 14:44:28 +0200

    Solved N178

---
+++ Niveau 1 - Grundlæggende C#/N178 Arrays (Simpel)/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int[] salaries = { 10000, 20000, 15000, 20000, 30000, 15000 };
+double averageSalary = (double)salaries.Sum() / salaries.Length;
+
+void PrintSalaries()
+{
+    for (int i = 0; i < salaries.Length; i++)
+    {
+        Console.WriteLine($"Salary {i + 1}: {salaries[i]:N0}");
+    }
+}
+
+
+PrintSalaries();
+Console.WriteLine($"Average: {averageSalary:N2}");
