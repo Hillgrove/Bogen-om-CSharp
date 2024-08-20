9c5063aaeb80e932b4fa7a6af85926de484839ff
Jesper Højlund <github@hillgrove.dk>
2024-08-20 17:00:49 +0200

    Solved N183

---
+++ Niveau 1 - Grundlæggende C#/N183 Bubblesort/Program.cs
@@ -0,0 +1,30 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] tal = { 5, 1, 4, 2, 8, 0, 2, 7, 6, 3, 9, 5, 8, 1, 4, 2, 7, 6, 3, 0 };
+        BubbleSort(tal);
+        Console.WriteLine("Sorted order:");
+        foreach (var item in tal)
+        {
+            Console.Write(item + " ");
+        }
+    }
+
+    private static void BubbleSort(int[] tal)
+    {
+        for (int i = 0; i < tal.Length - 1; i++)
+        {
+            for (int j = 0; j < tal.Length - i - 1; j++)
+            {
+                if (tal[j] > tal[j + 1])
+                {
+                    int temp = tal[j];
+                    tal[j] = tal[j + 1];
+                    tal[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
