21c06f60a243a38be04017b4f031fbcd3a72b275
Jesper Højlund <github@hillgrove.dk>
2024-08-24 17:40:52 +0200

    N200 Solved

---
+++ Niveau 2 - Videregående C#/N200 Simpel klasser (Terning)/Die.cs
@@ -0,0 +1,28 @@
+﻿
+public class Die
+{
+    public int faceValue;
+
+    public Die()
+    {
+        this.faceValue = 1;
+    }
+
+    public Die(int faceValue)
+    { 
+        if (faceValue < 1 || faceValue > 6)
+        {
+            throw new ApplicationException("Wrong value");
+        }
+        this.faceValue = faceValue; 
+    }
+
+
+    public void Shake()
+    {
+        Random rand = new Random();
+        faceValue = rand.Next(1, 7);
+    }
+
+}
+
--- Niveau 2 - Videregående C#/N200 Simpel klasser (Terning)/Program.cs
+++ Niveau 2 - Videregående C#/N200 Simpel klasser (Terning)/Program.cs
@@ -1,1 +1,14 @@
 ﻿
+Die d1 = new Die();
+Console.WriteLine($"Value of d1: {d1.faceValue}");
+
+Die d2 = new Die(3);
+Console.WriteLine($"Value of d2: {d2.faceValue}");
+
+d1.Shake();
+d2.Shake();
+Console.WriteLine($"Value of d1: {d1.faceValue}");
+Console.WriteLine($"Value of d2: {d2.faceValue}");
+
+// Will throw error
+d2 = new Die(7);
