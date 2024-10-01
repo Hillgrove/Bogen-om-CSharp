3112451dc3545ea15fb250515e4c976306786652
Jesper Højlund <github@hillgrove.dk>
2024-10-01 14:33:41 +0200

    feat: add N201

---
+++ Niveau 2 - Videregående C#/N201 Gæt et tal (simpel objektorienteret version)/Program.cs
@@ -0,0 +1,5 @@
+﻿
+using N201_Gæt_et_tal__simpel_objektorienteret_version_;
+
+Spil spil = new Spil();
+spil.Start();
---
+++ Niveau 2 - Videregående C#/N201 Gæt et tal (simpel objektorienteret version)/Spil.cs
@@ -0,0 +1,47 @@
+﻿
+namespace N201_Gæt_et_tal__simpel_objektorienteret_version_
+{
+    public class Spil
+    {
+        private int _tilfaeldigtTal;
+
+        public void GenerereTilfaeldigtTal()
+        {
+            Random random = new Random();
+            _tilfaeldigtTal = random.Next(1, 101);
+        }
+
+        public int HentTalFraBruger()
+        {
+            Console.WriteLine("Gæt et tal mellem 1 og 100: ");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void GivFeedback(int gaettetTal)
+        {
+            if (gaettetTal < _tilfaeldigtTal)
+            {
+                Console.WriteLine("For lavt!");
+            }
+            else if (gaettetTal > _tilfaeldigtTal)
+            {
+                Console.WriteLine("For højt!");
+            }
+            else
+            {
+                Console.WriteLine("Tillykke! Du har gættet rigtigt!");
+            }
+        }
+
+        public void Start()
+        {
+            GenerereTilfaeldigtTal();
+            int gaettetTal = 0;
+            while (gaettetTal != _tilfaeldigtTal)
+            {
+                gaettetTal = HentTalFraBruger();
+                GivFeedback(gaettetTal);
+            }
+        }
+    }
+}
