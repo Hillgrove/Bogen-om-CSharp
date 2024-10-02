7ddf49de7178ef963b7d7579c9ea9fe270d0352d
Jesper Højlund <github@hillgrove.dk>
2024-10-02 12:56:57 +0200

    feat: add and solve N203

---
+++ Niveau 2 - Videregående C#/N203 Leg med klasser/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using Dumpify;
+using MCronberg.Kursus;
+
+Spillekort s = new Spillekort();
+s.Dump();
+s.Vend();
+s.Dump();
+s.Vend();
+s.Dump();
+
+Spillekort s2 = new Spillekort(SpillekortKulør.Hjerter, SpillekortVærdi.Es);
+s2.Dump();
+
+SpillekortBunke b = new();
+b.TilføjKort(s);
+b.TilføjKort(s2);
+b.Dump();
+b.VendAlleKort();
+b.Dump();
+
+// Prøv også dette
+
+//SpillekortBunke b2 = SpillekortBunke.OpretBunke();
+//b2.Dump();
