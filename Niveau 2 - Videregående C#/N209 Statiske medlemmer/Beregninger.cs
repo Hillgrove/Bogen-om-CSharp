6e3bc99b6981ecbf8cf5790403bc106ac47e9e87
Jesper Højlund <github@hillgrove.dk>
2024-10-03 15:23:36 +0200

    feat: add and solve N209

---
+++ Niveau 2 - Videregående C#/N209 Statiske medlemmer/Beregninger.cs
@@ -0,0 +1,10 @@
+﻿
+namespace N209_Statiske_medlemmer
+{
+    public static class Beregninger
+    {
+        public static int LægSammen(int a, int b) => a + b;
+
+        public static int TrækFra(int a, int b) => a - b;
+    }
+}
---
+++ Niveau 2 - Videregående C#/N209 Statiske medlemmer/Program.cs
@@ -0,0 +1,7 @@
+﻿
+using N209_Statiske_medlemmer;
+
+int a = 5;
+int b = 3;
+int sum = Beregninger.LægSammen(a, b);
+
