7eb3b0e1f3be746cfc32a2b49e2f4644eef55c3d
Jesper Højlund <github@hillgrove.dk>
2024-10-03 15:09:02 +0200

    feat: add and solve N208

---
+++ Niveau 2 - Videregående C#/N208 Linked liste/MyLinkedList.cs
@@ -0,0 +1,44 @@
+﻿
+namespace N208_Linked_liste
+{
+    public class MyLinkedList
+    {
+        private Node? _head;
+
+        public void AddFirst(int data)
+        {
+            Node newNode = new Node(data);
+            newNode.Next = _head;
+            _head = newNode;
+        }
+
+        public void AddLast(int data)
+        {
+            Node newNode = new Node(data);
+            
+            if (_head == null)
+            {
+                _head = newNode;
+                return;
+            }
+
+            Node? current = _head;
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+
+            current.Next = newNode;            
+        }
+
+        public void PrintAllNodes()
+        {
+            Node current = _head;
+            while (current != null)
+            {
+                Console.WriteLine(current.Data);
+                current = current.Next;
+            }
+        }   
+    }
+}
---
+++ Niveau 2 - Videregående C#/N208 Linked liste/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using N208_Linked_liste;
+
+MyLinkedList linkedList = new MyLinkedList();
+linkedList.AddLast(1);
+linkedList.AddLast(2);
+linkedList.AddLast(3);
+linkedList.PrintAllNodes();
+
+Console.WriteLine("Indsætter 5 i starten.");
+linkedList.AddFirst(5);
+linkedList.PrintAllNodes();
