c3675fbfcc811f58db70ed320c0eab1d1d47e5bb
Jesper Højlund <github@hillgrove.dk>
2024-10-03 15:29:42 +0200

    feat: add and solve N215

---
+++ Niveau 2 - Videregående C#/N215 Hukommelsesteori 1/Program.cs
@@ -0,0 +1,18 @@
+﻿
+Person k1 = new Person();
+k1.Id = 1;
+Person k2 = new Person();
+k2.Id = 2;
+Console.WriteLine(k1.Id);
+Console.WriteLine(k2.Id);
+k1 = k2;
+Console.WriteLine(k1.Id);
+Console.WriteLine(k2.Id);
+k1.Id = 3;
+Console.WriteLine(k1.Id);
+Console.WriteLine(k2.Id);
+
+struct Person    // ret til class Person jf. opgave
+{
+    public int Id;
+}
