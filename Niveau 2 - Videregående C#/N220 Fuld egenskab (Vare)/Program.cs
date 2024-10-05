8136e66f9fd5ed27b18ac94752e813cb8c35a8b2
Jesper Højlund <github@hillgrove.dk>
2024-10-05 19:47:05 +0200

    feat: solve N220

--- Niveau 2 - Videregående C#/N220 Fuld egenskab (Vare)/Program.cs
+++ Niveau 2 - Videregående C#/N220 Fuld egenskab (Vare)/Program.cs
@@ -1,1 +1,33 @@
 ﻿
+Vare nyVare = new Vare();
+nyVare.Navn = "Banan";
+nyVare.Pris = 10;
+
+Console.WriteLine(nyVare.Navn);
+Console.WriteLine(nyVare.Pris);
+
+Console.WriteLine(nyVare.PrisMedMoms());
+
+class Vare
+{
+    private string _navn;
+
+    public string Navn
+    {
+        get { Console.WriteLine("Navn aflæst."); return _navn; }
+        set { Console.WriteLine($"Navn ændret til {value}"); _navn = value; }
+    }
+
+    private double _pris;
+
+    public double Pris
+    {
+        get { Console.WriteLine("Pris aflæst."); return _pris; }
+        set { Console.WriteLine($"Pris ændret til {value}"); _pris = value; }
+    }
+
+    public double PrisMedMoms()
+    {
+        return _pris * 1.25;
+    }
+}
