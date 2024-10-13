15a7892355f602592a53d91f6f1ad9bdb90db193
Jesper Højlund <github@hillgrove.dk>
2024-10-13 20:12:18 +0200

    feat: add and solve N225

---
+++ Niveau 2 - Videregående C#/N225 Fuld egenskab (Terning)/Program.cs
@@ -0,0 +1,48 @@
+﻿
+Dice newDice = new Dice();
+newDice.WriteFaceValue();
+newDice.Shake();
+newDice.WriteFaceValue();
+
+public class Dice
+{
+	private int _faceValue;
+
+	public int FaceValue
+	{
+		get
+        {
+            Console.WriteLine("Facevalue accessed.");
+            return _faceValue;
+        }
+		set
+		{
+			if (value < 1 || value > 6)
+                value = 1;
+            _faceValue = value;
+            Console.WriteLine($"Facevalue changed to {_faceValue}");
+        }
+	}
+
+    public Dice()
+    {
+        Shake();
+    }
+
+    public Dice(int faceValue)
+    {
+        FaceValue = faceValue;
+    }
+
+    public void Shake()
+    {
+        Random rnd = new Random();
+        FaceValue = rnd.Next(1, 7);
+    }
+
+    public void WriteFaceValue()
+    {
+        Console.WriteLine($"[{this.FaceValue}]");
+    }
+
+}
