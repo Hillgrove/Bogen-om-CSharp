186b5d574d21528ca609fa30223bf7bff4e6dc3e
Jesper Højlund <github@hillgrove.dk>
2024-10-20 21:54:14 +0200

    feat: add N227

---
+++ Niveau 2 - Videregående C#/N227 Fuld egenskab (fejl)/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Indkapsling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            p.Navn = "x";
+            Console.WriteLine(p.Navn);
+        }
+    }
+
+    class Person
+    {
+        private string navn;
+
+        public string Navn
+        {
+            get { return navn; }
+            set { Navn = value; }
+        }
+    }
+}
