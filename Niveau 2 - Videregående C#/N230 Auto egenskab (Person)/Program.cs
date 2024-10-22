22c4deff5745014f72698eb65a6c24db9342a7eb
Jesper Højlund <github@hillgrove.dk>
2024-10-22 16:10:48 +0200

    feat: solve N230

--- Niveau 2 - Videregående C#/N230 Auto egenskab (Person)/Program.cs
+++ Niveau 2 - Videregående C#/N230 Auto egenskab (Person)/Program.cs
@@ -1,1 +1,39 @@
 ﻿
+Person newPerson = new Person();
+newPerson.FirstName = "Test";
+newPerson.LastName = "McTesterson";
+
+Console.WriteLine(newPerson.FullName());
+
+newPerson.LastName = "nul";
+Console.WriteLine(newPerson.FullName());
+
+
+class Person
+{
+	public string? FirstName { get; set; }
+	private string? _lastName;
+
+	public string? LastName
+	{
+		get { return _lastName; }
+        set
+        {
+            if (value != null && value.Length <= 3)
+            {
+                _lastName = "";
+            }
+
+            else
+            {
+                _lastName = value;
+            }
+            
+        }
+    }
+
+	public string FullName()
+	{
+		return $"{FirstName} {LastName}";
+	}
+}
