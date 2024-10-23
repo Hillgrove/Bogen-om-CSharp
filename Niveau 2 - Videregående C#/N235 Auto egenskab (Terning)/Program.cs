cce9d57064d120dd2ae0ee142eaaa2458d782684
Jesper Højlund <github@hillgrove.dk>
2024-10-23 21:49:11 +0200

    feat: add and solve N235

---
+++ Niveau 2 - Videregående C#/N235 Auto egenskab (Terning)/Program.cs
@@ -0,0 +1,50 @@
+﻿
+Console.WriteLine("First Die:");
+Die firstDie = new Die();
+firstDie.ShowDie();
+
+Console.WriteLine("\nSecond Die:");
+Die secondDie = new Die(3);
+secondDie.ShowDie();
+secondDie.Shake();
+secondDie.ShowDie();
+secondDie.Shake();
+secondDie.ShowDie();
+secondDie.Shake();
+secondDie.ShowDie();
+secondDie.Shake();
+secondDie.ShowDie();
+secondDie.Shake();
+secondDie.ShowDie();
+
+
+public class Die
+{
+    public int FaceValue { get; private set; }
+
+    public Die()
+    {
+        Shake();
+    }
+
+    public Die(int faceValue)
+    {
+        if (faceValue < 1 || faceValue > 7)
+        {
+            throw new ArgumentOutOfRangeException(nameof(faceValue));
+        }
+
+        FaceValue = faceValue;
+    }
+
+    public void Shake()
+    {
+        Random random = new Random();
+        FaceValue = random.Next(1, 7);
+    }
+
+    public void ShowDie()
+    {
+        Console.WriteLine($"[{FaceValue}]");
+    }
+}
