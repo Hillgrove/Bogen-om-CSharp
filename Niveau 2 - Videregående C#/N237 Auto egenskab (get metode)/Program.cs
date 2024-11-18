426e636ec4e20a3799d3d99557475f81655c40f5
Jesper Højlund <github@hillgrove.dk>
2024-11-18 18:48:25 +0100

    feat: add and solve N237

---
+++ Niveau 2 - Videregående C#/N237 Auto egenskab (get metode)/Program.cs
@@ -0,0 +1,19 @@
+﻿
+var triangle = new Triangle(5, 5);
+Console.WriteLine(triangle.Area);
+
+public class Triangle
+{
+    public int Base { get; private set; }
+    public int Height { get; private set; }
+    public double Area { get { return Base * Height * 0.5; }}
+
+
+    public Triangle(int triangleBase, int height)
+    {
+        Base = triangleBase;
+        Height = height;
+    }
+}
+
+
